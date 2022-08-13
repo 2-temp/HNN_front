b5321d557b0d864b61419f77241508a471a492e1
Dusuna <dusunax@gmail.com>
2022-08-13 09:52:52 +0900

    MOCK API(RESPONSE.js)에 유저, 코멘트 추가

--- src/RESPONSE.js
+++ src/RESPONSE.js
@@ -1,85 +1,144 @@
 const data={
     LOGIN: {
         success: true,
-        token: "testCookieString",
-        // success: false,
-        // msg: "회원 정보가 일치하지 않습니다."
+        token: "testCookieString"
+    },
+    LOGIN_FAIL: {
+        success: false,
+        msg: "회원 정보가 일치하지 않습니다."
     },
     LOGOUT: {
         success: true
     },
+    SIGNIN: {
+        msg : "회원가입을 축하드립니다!",
+        success: true
+    },
+    EMAIL_CHECK: {
+        msg : "사용할 수 있는 닉네임입니다.",
+        success: true
+    },
+    PASSWORD_CHECK: {
+        msg : "사용할 수 있는 이메일입니다.",
+        success: true
+    },
+    USER_PROFILE: [
+        {
+            userId: 0,
+            email: "test@email.com",
+            nickname: "닉네임0",
+            MBTI: "MBTI",
+            profilePicture: "프로필사진URL",
+        },
+        {
+            userId: 1,
+            email: "test@email.com",
+            nickname: "닉네임1",
+            MBTI: "MBTI",
+            profilePicture: "프로필사진URL",
+        },
+        {
+            userId: 2,
+            email: "test@email.com",
+            nickname: "닉네임2",
+            MBTI: "MBTI",
+            profilePicture: "프로필사진URL",
+        },
+    ],
     POSTS: [
         {  
-            title: "게시글 제목1",
-            content: "게시글 내용1",
-            nickname: "닉네임1",
-            profilePicture: "프로필사진URL",
+            postId: 0,
+            title: "게시글 제목0",
+            imageUrl: "post_img.jpg",
+            content: "게시글 내용0",
+            createdAt: "2022-99-99, 99:99",
+            like: 9999,
+            countComments: 9999,
+            
+            email: "test@email.com",
+            nickname: "닉네임0",
             MBTI: "MBTI",
-            createdAt: "2022-08-10, 21:00",
-            like: 1111,
-            countComments: 1111
-        },
-        {
-            title: "게시글 제목2",
-            content: "게시글 내용2",
-            nickname: "닉네임2",
             profilePicture: "프로필사진URL",
-            MBTI: "MBTI",
-            createdAt: "2022-08-11, 21:00",
-            like: 2222,
-            countComments: 2222
-        },
-        {
-            title: "게시글 제목3",
-            content: "게시글 내용3",
-            nickname: "닉네임3",
-            profilePicture: "프로필사진URL",
-            MBTI: "MBTI",
-            createdAt: "2022-08-12, 21:00",
-            like: 3333,
-            countComments: 3333
         },
         {  
+            postId: 1,
             title: "게시글 제목1",
+            imageUrl: "post_img.jpg",
             content: "게시글 내용1",
+            createdAt: "2022-99-99, 99:99",
+            like: 9999,
+            countComments: 9999,
+            
+            email: "test@email.com",
             nickname: "닉네임1",
-            profilePicture: "프로필사진URL",
             MBTI: "MBTI",
-            createdAt: "2022-08-10, 21:00",
-            like: 1111,
-            countComments: 1111
+            profilePicture: "프로필사진URL",
         },
-        {
+        {  
+            postId: 2,
             title: "게시글 제목2",
+            imageUrl: "post_img.jpg",
             content: "게시글 내용2",
+            createdAt: "2022-99-99, 99:99",
+            like: 9999,
+            countComments: 9999,
+            
+            email: "test@email.com",
             nickname: "닉네임2",
-            profilePicture: "프로필사진URL",
             MBTI: "MBTI",
-            createdAt: "2022-08-11, 21:00",
-            like: 2222,
-            countComments: 2222
+            profilePicture: "프로필사진URL",
         },
-        {
+        {  
+            postId: 3,
             title: "게시글 제목3",
+            imageUrl: "post_img.jpg",
             content: "게시글 내용3",
+            createdAt: "2022-99-99, 99:99",
+            like: 9999,
+            countComments: 9999,
+            
+            email: "test@email.com",
             nickname: "닉네임3",
-            profilePicture: "프로필사진URL",
             MBTI: "MBTI",
-            createdAt: "2022-08-12, 21:00",
-            like: 3333,
-            countComments: 3333
+            profilePicture: "프로필사진URL",
         }
     ],
-    DETAIL: {
-        title: "게시글 제목3",
-        content: "게시글 내용3",
-        nickname: "닉네임3",
-        profilePicture: "프로필사진URL",
+    COMMENTS: [
+        {
+            commentId: 0,
+            content: "댓글입니다0"
+        },
+        {
+            commentId: 1,
+            content: "댓글입니다0"
+        },
+        {
+            commentId: 2,
+            content: "댓글입니다0"
+        },
+        {
+            commentId: 3,
+            content: "댓글입니다0"
+        },
+        {
+            commentId: 4,
+            content: "댓글입니다0"
+        }
+    ],
+    DETAIL: {  
+        postId: 0,
+        title: "게시글 제목0",
+        imageUrl: "post_img.jpg",
+        content: "게시글 내용0",
+        createdAt: "2022-99-99, 99:99",
+        like: 9999,
+        countComments: 9999,
+        
+        email: "test@email.com",
+        nickname: "닉네임0",
         MBTI: "MBTI",
-        createdAt: "2022-08-12, 21:00",
-        like: 3333,
-        countComments: 3333
-    }
+        profilePicture: "프로필사진URL",
+    },
 }
 
 export default data
