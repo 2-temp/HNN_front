5edfedfa16abb46ee0282c33ea2e80f53095ef00
Dusuna <dusunax@gmail.com>
2022-08-15 13:00:54 +0900

    메인페이지에 페이지네이션 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,31 +9,61 @@ import Detail from "./Detail";
 
 function Main() {
   const [posts, setPosts] = useState([]);
-  const [limit, setLimit] = useState(10);
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-  // let data = RESPONSE.POSTS;
-  // setPosts(data)
+  const [limit, setLimit] = useState(10);
 
-  // "https://jsonplaceholder.typicode.com/posts"
-  
-  
   useEffect(() => {
-    let data = RESPONSE.POSTS;
-    // let data = axios.get('/posts')
-    setPosts(data);
+    setLoading(true)
+
+    const fetchData = async () => {
+      // const data = await axios.get('/posts')
+      const data = RESPONSE.POSTS;
+      setPosts(data);
+      setLoading(false);
+    };
+    fetchData();
   })
+
+  const indexLast = page * limit; // 1 * 10 / 2 * 10
+  const indexFirst = indexLast - limit; // 10 - 10 / 20 - 10
+  const current = (posts) => {
+    let current = 0;
+    current = posts.slice(indexFirst, indexLast);
+    return current;
+  }  
 
   return (
     <MainPost>
       <h4>전체 글 목록</h4>
+
+      <ul className="set_page_limit">
+        <li 
+          onClick={()=>setLimit(3)}
+          className={limit==3?"active":""}
+        >
+            3개 보기
+        </li>
+        <li 
+          onClick={()=>setLimit(10)}
+          className={limit==10?"active":""}
+        >
+            10개 보기
+        </li>
+      </ul>
+      
       <div className="posts_box">
-        { posts.map((list, i) => {
+        {!loading && current(posts).map((list, i) => {
           return <Article list={list} key={i} /> 
-        })
-        }
+        })}
       </div>
 
-      <Pagination />
+      <Pagination 
+        totalPost = {posts.length}
+        limit = {limit}
+        page = {page}
+        setPage = {setPage}
+      />
 
     </MainPost>
   )
@@ -47,6 +77,20 @@ const MainPost = styled.div`
     min-height: calc(100vh - 200px);
   }
 
+  .set_page_limit {
+    font-size: 10px;
+    display: flex;
+    gap: 10px;
+    margin: 10px 0;
+    
+    li {
+      cursor: pointer;
+    }
+    li.active {
+      color: orange;
+    }
+  }
+
   .pagination {
     text-align: center;
 
@@ -58,7 +102,13 @@ const MainPost = styled.div`
     }
 
     button.active {
-      border-bottom: 2px solid #222;
+      color: orange;
+      border-bottom: 2px solid orange;
+    }
+
+    button.unactive {
+      opacity: .3;
+      pointer-events: none;
     }
   }
 `
