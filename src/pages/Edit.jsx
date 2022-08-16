c7008530f79bf77fc96efd492123181aaba88f86
minongarm2arm <constant_347@naver.com>
2022-08-16 22:21:41 +0900

    게시물 수정 기능 추가

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -6,111 +6,139 @@ import RESPONSE from "../RESPONSE";
 import { useNavigate } from "react-router-dom";
 import data from "../RESPONSE";
 import { useParams } from "react-router-dom";
+import { getCookie } from '../cookie';
 
 function Edit() {
+  //쿠키 가져옴
+  const token = getCookie("token");
 
   const navigate = useNavigate();
 
   const { postId } = useParams();
-  const editList = data.POSTS[postId]
+  const editList = data.POSTS[postId];
 
-  console.log(editList)
+  console.log(editList);
 
-  // 변경된 게시물 내용, 이미지, 가수, 노래명을 저장할 곳 선언
+  // 변경된 가수, 노래명, 이미지, 게시물 내용을 저장할 곳 선언
   const [editInputs, seTeditInputs] = useState({
-    content: editList.content,
-    imageUrl: editList.imageUrl,
-    singer: editList.info.singer,
     songTitle: editList.info.songTitle,
+    singer: editList.info.singer,
+    imageUrl: editList.imageUrl,
+    content: editList.content,
   });
 
   const { content, imageUrl, songTitle, singer } = editInputs;
+
+  const onChange = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    seTeditInputs({
+      ...editInputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
+  };
 
   // edit 핸들러 설정
   const oneEditHandler = async (event) => {
     event.preventDefault();
 
     // 채워지지 않은 칸이 있을 경우
-    if (content === '' || imageUrl === '' || songTitle === '' || singer === '') {
-      alert('빈칸이 있습니다 !')
+    if (
+      content === "" ||
+      imageUrl === "" ||
+      songTitle === "" ||
+      singer === ""
+    ) {
+      alert("빈칸이 있습니다 !");
       return;
     }
 
+    // 게시물 수정 버그
     // let count = 0;
 
     // 수정될 때마다 count++
-    for (const x in editInputs) {
-      if (editInputs[x] !== editList[x]) {
-        console.log(editInputs[x], editList[x]);
-        count++;
-        // alert("변경사항 있음");
-      }
-    }
+    // for (const x in editInputs) {
+    //   if (editInputs[x] !== editList[x]) {
+    //     console.log(editInputs[x], editList[x]);
+    //     count++;
+    //     alert("변경사항 있음");
+    //   }
+    // }
 
-    if(count === 0) {
-      // alert("변경된 사항이 없습니다.");
-    }
+    // if(count === 0) {
+    //   alert("변경된 사항이 없습니다.");
+    // }
 
-    const new_data = { data, editInputs }
-    console.log(new_data)
+    const new_data = { data, editInputs };
+    console.log(new_data);
 
-    const response = RESPONSE.EDIT_CHECK
-    console.log(response)
+    const response = RESPONSE.EDIT_CHECK;
+    console.log(response);
     if (response.success) {
-      alert(response.msg)
-      navigate(`/post/${postId}`)
+      alert(response.msg);
+      navigate(`/post/${postId}`);
     } else {
-      alert(response.msg)
-      navigate(`/post/${postId}`)
+      alert(response.msg);
+      navigate(`/post/${postId}`);
     }
+
+    /////////////////////////////////////////////////////////////
+    // post 요청
+const oneEditHandler = async (event) => {
+  event.preventDefault();
+  await axios.post('http://gwonyeong.shop/post', editInputs, {
+  headers: {
+  authorization: `Bearer ${token}`
   }
+  }).then(res => {
+  console.log(res)
+  console.log(res.data)
+  })
+  };
 
   return (
     <Contents>
-      <form onSubmit={(event) => { oneEditHandler(event) }}>
+      <form
+        onSubmit={(event) => {
+          oneEditHandler(event)
+        }}
+      >
         <h3>글 수정</h3>
 
-        <input onChange={(ev) => {
-          seTeditInputs({
-            ...editInputs,
-            imageUrl: ev.target.value,
-          });
-        }}
+        <input
+          onChange={onChange}
           value={imageUrl}
-          name='imageUrl'
+          name="imageUrl"
           placeholder="이미지 Url"
         />
 
-        <input onChange={(ev) => {
-          seTeditInputs({
-            ...editInputs,
-            content: ev.target.value,
-          });
-        }}
+        <input
+          onChange={onChange}
           value={content}
-          name='content'
+          name="content"
           placeholder="게시물 내용"
         />
 
-        <input onChange={(ev) => {
-          seTeditInputs({
-            ...editInputs,
-            songTitle: ev.target.value,
-          });
-        }} value={songTitle} name='songTitle' placeholder="노래 이름" />
+        <input
+          onChange={onChange}
+          value={songTitle}
+          name="songTitle"
+          placeholder="노래 이름"
+        />
 
-        <input onChange={(ev) => {
-          seTeditInputs({
-            ...editInputs,
-            singer: ev.target.value,
-          });
-        }} value={singer} name='singer' placeholder="가수" />
-        
+        <input
+          onChange={onChange}
+          value={singer}
+          name="singer"
+          placeholder="가수"
+        />
+
         <button>수정하기</button>
       </form>
     </Contents>
-  )
+  );
 }
+}
+
 export default Edit;
 
 const Contents = styled.div`
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,72 +3,62 @@ import styled from "styled-components";
 import { useState } from "react";
 import axios from "axios";
 import RESPONSE from "../RESPONSE";
-import data from "../RESPONSE";
 import { useNavigate } from "react-router-dom";
+import { getCookie } from '../cookie';
 
 
 function Post() {
+// 쿠키 가져옴
+const token = getCookie('token');
 
-  const navigate = useNavigate();
+const navigate = useNavigate();
 
-  //값들을 저장시켜 놓을 usestate 생성
-  const [inputs, setInputs] = useState({
-    title: "",
-    content: "",
-    imageUrl: "",
-    songTitle:"",
-    singer:"",
+// 값들을 저장시켜 놓을 usestate 생성
+const [inputs, setInputs] = useState({
+content: "",
+imageUrl: "",
+songTitle:"",
+singer:"",
+});
+
+const { content, imageUrl, songTitle, singer } = inputs;
+
+const onChange = (e) => {
+  const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+  setInputs({
+    ...inputs, // 기존의 input 객체를 복사한 뒤
+    [name]: value, // name 키를 가진 값을 value 로 설정
   });
+};
 
-  const { title, content, imageUrl, songTitle, singer } = inputs;
-
-  const onChange = (e) => {
-    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-    setInputs({
-      ...inputs, // 기존의 input 객체를 복사한 뒤
-      [name]: value, // name 키를 가진 값을 value 로 설정
+// post 요청
+const onClickEditButtonHandler = async (event) => {
+  event.preventDefault();
+  await axios
+    .post("http://gwonyeong.shop/post", inputs, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      console.log(res.data);
     });
-  };
+};
 
-//onClick 함수 생성
-  const onClickEditButtonHandler = async (event) => {
-    event.preventDefault();
-  
-    const new_data = { inputs }
-    console.log(inputs)
+return (
+<Contents>
+<form onSubmit={(event) => { onClickEditButtonHandler(event) }}>
+<h3>글 작성</h3>
 
-    // const response = await axios.post("/post", null, {
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //     }
-    // });
-
-    //성공 여부에 따라 나올 메세지
-    const response = RESPONSE.POST_CHECK
-    console.log(response)
-    if(response.success) {
-      alert(response.msg)
-      navigate('/')
-    }else {
-      alert(response.msg)
-      navigate('/sign/in')
-    }
-  } 
-
-  return (
-    <Contents>
-      <form onSubmit={(event) => { onClickEditButtonHandler(event) }}>
-        <h3>글 작성</h3>
-
-        <input onChange={onChange} minLength={1} value={singer} name='singer' placeholder="가수명"/>
-        <input onChange={onChange} minLength={1} value={songTitle} name='songTitle' placeholder="곡 제목"/>
-        <input onChange={onChange} minLength={1} value={title} name='title' placeholder="제목"/>
-        <input onChange={onChange} minLength={1} value={imageUrl} name='imageUrl' placeholder="이미지 Url"/>
-        <input onChange={onChange} minLength={5} value={content} name='content' placeholder="게시물 내용"/>
-        <button>작성하기</button>
-      </form>
-    </Contents>
-  )
+<input onChange={onChange} minLength={1} value={songTitle} name='songTitle' placeholder="곡 제목"/>
+<input onChange={onChange} minLength={1} value={singer} name='singer' placeholder="가수명"/>
+<input onChange={onChange} minLength={1} value={imageUrl} name='imageUrl' placeholder="이미지 Url"/>
+<input onChange={onChange} minLength={5} value={content} name='content' placeholder="게시물 내용"/>
+<button>작성하기</button>
+</form>
+</Contents>
+)
 }
 
 export default Post;
@@ -80,34 +70,34 @@ padding: 0 20px;
 box-sizing: border-box;
 
 form {
-    max-width: 600px;
-    margin: 0 auto;
+max-width: 600px;
+margin: 0 auto;
 
-    display: flex;
-    flex-flow: column;
-    gap: 16px;
+display: flex;
+flex-flow: column;
+gap: 16px;
 
-    text-align: center;
+text-align: center;
 
-    h3 {
-      font-size: 28px;
-    }
+h3 {
+font-size: 28px;
+}
 
-    input, button {
-      font-size: 18px;
-      padding: 6px 26px;
-      box-sizing: border-box;
-      border-radius: 20px;
+input, button {
+font-size: 18px;
+padding: 6px 26px;
+box-sizing: border-box;
+border-radius: 20px;
 
-      border: none;
-      box-shadow: 2px 2px 5px #ddd;
+border: none;
+box-shadow: 2px 2px 5px #ddd;
 
-      transition: all .2s;
-    }
-    
-    button:hover {
-      background-color: #ccc;
-      cursor: pointer;
-    }
-  }
+transition: all .2s;
+}
+
+button:hover {
+background-color: #ccc;
+cursor: pointer;
+}
+}
 `
